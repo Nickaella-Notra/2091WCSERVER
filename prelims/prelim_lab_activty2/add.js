9fb38ca7901c2830b0790d97046dabe667036266
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-01-23 20:51:00 +0800

    2 files added

---
+++ prelims/prelim_lab_activty2/add.js
@@ -0,0 +1,7 @@
+//Name: Nickaella Notra
+//WD-201
+
+function add(val1, val2) {
+  return val2 + val2;
+}
+module.exports = add;
---
+++ prelims/prelim_lab_activty2/app.js
@@ -0,0 +1,8 @@
+//Name: Nickaella Notra
+//WD-201
+
+const add = require('./add.js');
+var val1 = 4;
+var val2 = 3;
+
+console.log(add(val1, val2));
