4272ff50cddb3af16f33fe525dd1f4bc86c9c05e
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-01-30 12:51:32 +0800

    1 file added, 1 deleted

--- prelims/Lab-Act-3-NodeJS-Server.js
+++ prelims/Lab-Act-3-NodeJS-Server.js
@@ -8,10 +8,9 @@ var server = http.createServer(function (req, res) {
   // if (req.url == '/');
   // {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.wrie(
+  res.write(
     '<html><body><p>Welcome to my Node.js Application</p><p>Welcome Nicka. This is an activty about basics of Node.js</p></body></html>'
   );
-  res.end();
   // }
   // else if(req.url == '/about')
   // {
