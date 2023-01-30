74a1e6795bceb9ee3681a67390b2274d05cca059
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-01-30 12:38:56 +0800

    1 file modified

--- prelims/LAB-Activtiy-3/Lab-Act-3-NodeJS-Server.js
+++ prelims/LAB-Activtiy-3/Lab-Act-3-NodeJS-Server.js
@@ -2,4 +2,15 @@
 //Author: Nickaella Notra
 //Section: WD-201
 
-var 
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  if (req.url == '/');
+  {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.wrie(
+      '<html><body><p>Welcome to my Node.js Application</p><p>Welcome Nicka. This is an activty about basics of Node.js</p></body></html>'
+    );
+    req.end();
+  }
+});
