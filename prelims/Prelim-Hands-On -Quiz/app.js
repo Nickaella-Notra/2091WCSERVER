e9b2d435f44f1ab3729fd89cbc3683adf8c90123
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-01-30 23:17:00 +0800

    3 files added, 1 deleted

---
+++ prelims/Prelim-Hands-On -Quiz/app.js
@@ -0,0 +1,25 @@
+//Task Name: Hands-On Quiz 1
+//Author: Nickaella Notra
+//Section: WD-201
+
+const calc = require('./calculation.js');
+
+const rateHR = 300;
+const days = 6;
+const hrs = 4;
+const workHR = calc.multiply(days, hrs);
+
+const gross = calc.multiply(workHR, rateHR);
+const tax = calc.multiply(gross, 0.1);
+const sss = 1200;
+const pag_ibig = 300;
+const philH = 400;
+const deduct = calc.add(calc.add(tax, sss), calc.add(pag_ibig, philH));
+
+console.log('The gross income is ' + gross);
+console.log('Tax: ' + tax);
+console.log('SSS: ' + sss);
+console.log('Pag-Ibig fund: ' + pag_ibig);
+console.log('PhilHealth: ' + philH);
+console.log('Total deductions: ' + deduct);
+console.log('The net salary is ' + calc.subtract(gross, deduct));
---
+++ prelims/Prelim-Hands-On -Quiz/calculation.js
@@ -0,0 +1,27 @@
+//Task Name: Hands-On Quiz 1
+//Author: Nickaella Notra
+//Section: WD-201
+
+var calc = {
+  add: function (a, b) {
+    let x = a + b;
+    return x;
+  },
+
+  subtract: function (a, b) {
+    let x = a - b;
+    return x;
+  },
+
+  multiply: function (a, b) {
+    let x = a * b;
+    return x;
+  },
+
+  divide: function (a, b) {
+    let x = a / b;
+    return x;
+  },
+};
+
+module.exports = calc;
