93ca4d3244f856f04ad2ae7016bd13f24e710967
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-01-30 13:05:31 +0800

    1 file modified

--- prelims/Lab-Act-3-NodeJS-Server.js
+++ prelims/Lab-Act-3-NodeJS-Server.js
@@ -2,32 +2,66 @@
 //Author: Nickaella Notra
 //Section: WD-201
 
+// var http = require('http');
+
+// var server = http.createServer(function (req, res) {
+//   // if (req.url == '/');
+//   // {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write(
+//     '<html><body><p>Welcome to my Node.js Application</p><p>Welcome Nicka. This is an activty about basics of Node.js</p></body></html>'
+//   );
+//   // }
+//   // else if(req.url == '/about')
+//   // {
+//   //   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   //   res.wrie(
+//   //     '<html><body><p>This is the About Page</p><p>Hello Nicka. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>');
+//   //   res.end();
+//   // }
+//   // else if(req.url == '/contact')
+//   // {
+//   //   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   //   res.wrie(
+//   //     '<html><body><p>This is the Contact Page</p><p>Nicka, if you want additonal details about this activity go to site </p></body></html>');
+//   //   res.end();
+//   // else
+//   //   res.end('Invalid Request!');
+// });
+
+// // server.listen(5000);
+// console.log('Node.js server at port 5000 is running...');
+
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-  // if (req.url == '/');
-  // {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(
-    '<html><body><p>Welcome to my Node.js Application</p><p>Welcome Nicka. This is an activty about basics of Node.js</p></body></html>'
-  );
-  // }
-  // else if(req.url == '/about')
-  // {
-  //   res.writeHead(200, { 'Content-Type': 'text/html' });
-  //   res.wrie(
-  //     '<html><body><p>This is the About Page</p><p>Hello Nicka. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>');
-  //   res.end();
-  // }
-  // else if(req.url == '/contact')
-  // {
-  //   res.writeHead(200, { 'Content-Type': 'text/html' });
-  //   res.wrie(
-  //     '<html><body><p>This is the Contact Page</p><p>Nicka, if you want additonal details about this activity go to site </p></body></html>');
-  //   res.end();
-  // else
-  //   res.end('Invalid Request!');
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
+      '<html><body><h1>Welcome to my Node.js Application</h1><p>Welcome Nicka. This is an activty about basics of Node.js</p></body></html>'
+    );
+  } else if (req.url == '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write(
+      '<html><body><h1>This is the About Page</h1><p>Hello Nickaella Notra. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write(
+      '<html><body><h1>This is the Contact Page</h1><p>Nickaella Notra, if you want additional details about this activity go to this site: https://www.tutorialteacher.com/nodejs/nodejs-tutorials</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><h1>This is the Gallery Page</h1></body></html>');
+    res.end();
+  } else {
+    res.end('Invalid Request');
+  }
 });
 
-// server.listen(5000);
+server.listen(5000);
 console.log('Node.js server at port 5000 is running...');
