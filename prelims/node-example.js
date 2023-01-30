29933a20124516282b8d13d91a63a022eb67a64d
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-01-30 23:24:22 +0800

    1 file modified, 2 added, 2 deleted

--- prelims/node-example.js
+++ prelims/node-example.js
@@ -3,11 +3,11 @@
 //Week 2 and 3: Basics of NodeJS
 
 //First Program
-console.log("Welcome to WCSERVER");
+console.log('Welcome to WCSERVER');
 
-//Creating my first function 
+//Creating my first function
 function Display(x) {
-    console.log(x);
+  console.log(x);
 }
 
-Display(200) 
+Display(200);
