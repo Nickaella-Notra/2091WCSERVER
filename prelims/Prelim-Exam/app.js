18e345fc840dc7882c2d2d6167475d8ba4595942
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-02-08 15:48:22 +0800

    2 files added

---
+++ prelims/Prelim-Exam/app.js
@@ -0,0 +1,13 @@
+//Task Name: Prelim Exam 
+//Author: Nickaella Notra
+//Section: WD-201
+
+var message = require('./message.js');
+
+console.log(message.greet('Nicka'));
+console.log(message.congratulations('Nicka'));
+console.log(message.farewell('Nicka'));
+
+
+
+
---
+++ prelims/Prelim-Exam/message.js
@@ -0,0 +1,21 @@
+//Task Name: Prelim Exam 
+//Author: Nickaella Notra
+//Section: WD-201
+
+var message = 
+{
+  greet: function(name)
+  {
+    return "Hello, " + name + "!";
+  },
+  congratulations: function(name)
+  {
+    return "Congratulations, " + name + "!";
+  },
+  farewell: function(name)
+  {
+    return "Goodbye, " + name + "!";
+  }
+}
+
+module.exports(message);
