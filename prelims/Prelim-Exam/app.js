364b3a9f7713ae1350a286ca828c82e1a706e82e
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-02-08 15:49:18 +0800

    1 file modified

--- prelims/Prelim-Exam/app.js
+++ prelims/Prelim-Exam/app.js
@@ -1,13 +1,7 @@
-//Task Name: Prelim Exam 
+//Task Name: Prelim Exam
 //Author: Nickaella Notra
 //Section: WD-201
 
 var message = require('./message.js');
 
-console.log(message.greet('Nicka'));
-console.log(message.congratulations('Nicka'));
-console.log(message.farewell('Nicka'));
-
-
-
-
+console.log(message('Nicka'));
