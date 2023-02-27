8062d0fef0e0579cc7c9656f966e87d171a024f4
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-02-27 12:26:38 +0800

    1 file modified

--- midterms/express-app/get-method/server.js
+++ midterms/express-app/get-method/server.js
@@ -15,6 +15,7 @@ app.get('/process_get', function (req, res) {
   console.log(response);
   res.end(JSON.stringify(response));
 });
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
