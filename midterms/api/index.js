0ebc8412b97f545fe8ef280ece3335c619be35f3
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-03-06 21:31:43 +0800

    1 file modified

--- midterms/api/index.js
+++ midterms/api/index.js
@@ -5,4 +5,20 @@ app.get('/', (req, res) => {
   res.send('My New App!');
 });
 
+app.get('/api/heroes', (req, res) => {
+  res.send(['Superman', 'Iron Man', 'Batman', 'Hulk']);
+});
+
+app.get('/api/heroes/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get('/api/heroes/:title/:publisher', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/api/heroes/:title/:publisher', (req, res) => {
+  res.send([req.params, req.query]);
+});
+
 app.listen(3000, () => console.log('listening on port 3000'));
