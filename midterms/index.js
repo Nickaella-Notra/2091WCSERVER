ac55ecff31db50c83e9bfb2bde21030e667cebaf
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-03-13 12:18:56 +0800

    1 file modified

--- midterms/index.js
+++ midterms/index.js
@@ -4,9 +4,11 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'My EJS App'});
+  res.render('index', { title: 'My EJS App' });
 });
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+app.use(express.static('public'));
