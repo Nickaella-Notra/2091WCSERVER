c466abb3463487a0af0a361a8dddf622237656ab
Nickaella-Notra <122892899+Nickaella-Notra@users.noreply.github.com>
2023-03-22 15:44:38 +0800

    1 file added

---
+++ midterms/midterm-exam/exam.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const dishes = [{
+  type: 'Sisig',
+  province: 'Pampanga',
+  price: 220
+},
+{
+  type: 'Salpicao',
+  province: 'Quezon',
+  price: 180
+},
+{
+  type: "Bagnet",
+  province: 'Ilocos',
+  price: 370
+}];
+
+app.get('/dishes', (req, res) => {
+  res.send(dishes);
+});
+
+app.get('/dishes/:type', (req, res) => {
+  res.send(req.params.type);
+});
+
+app.get('/dishes/:type', (req, res) => {
+  const dish = dishes.find(h => h.type === parseInt(req.params.type));
+  if(!dish) return res.status(404).send('Record not Found');
+  res.send(dishes);
+});
+
+app.listen(3000, () => console.log('listening on port 3000'));
